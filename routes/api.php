c6b6ba166242cb604887b4c38609749fd22441c3
Henni Karam <karam.tenes@gmail.com>
2019-02-09 16:57:10 +0100

    Define api routes for login and register

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,20 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['jwt.auth','api-header']], function () {
+  
+    Route::get('users/list', function(){
+        $users = App\User::all();
+        
+        $response = ['success'=>true, 'data'=>$users];
+        return response()->json($response, 201);
+    });
+
+});
+
+Route::group(['middleware' => 'api-header'], function () {
+
+    Route::post('user/login', 'UserController@login');
+    Route::post('user/register', 'UserController@register');
+});
