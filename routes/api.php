db05a0748beaa2865f02890ba0556182fd62ec8c
Henni Karam <karam.tenes@gmail.com>
2019-03-01 08:31:46 +0100

    Commit for push

--- routes/api.php
+++ routes/api.php
@@ -17,19 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['jwt.auth','api-header']], function () {
-  
-    Route::get('users/list', function(){
-        $users = App\User::all();
-        
-        $response = ['success'=>true, 'data'=>$users];
-        return response()->json($response, 201);
-    });
-
-});
 
 Route::group(['middleware' => 'api-header' ,'namespace' => 'User' , 'prefix' => 'user'], function () {
 
-    Route::post('/login', 'User\UserController@login')->name('login');
-    Route::post('/register', 'User\UserController@register');
+    Route::post('/login', 'UserController@login')->name('login');
+    Route::post('/register', 'UserController@register');
 });
